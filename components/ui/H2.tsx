b9c2e4ab2aadb4350fa9235c33b3af5ddc8dccae
Upvisor <contacto.upvisor@gmail.com>
2024-01-30 22:05:57 -0300

    actualization general

--- components/ui/H2.tsx
+++ components/ui/H2.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const H2 = ({ children, config }: { children: React.ReactNode, config?: string }) => {
   return (
-    <h2 className={`${config} text-[20px] font-semibold tracking-widest lg:text-[24px]`}>{ children }</h2>
+    <h2 className={`${config} text-[20px] font-medium tracking-wide lg:text-[24px]`}>{ children }</h2>
   )
 }
