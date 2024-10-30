2d325da24bdaec011e029b16afee6db3b3586e0b
Upvisor <contacto.upvisor@gmail.com>
2024-10-30 14:31:30 -0300

    actualization general

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -2,13 +2,15 @@ import React from "react"
 import { Design, ICall, IForm, IFunnel, IPayment, IPolitics, IService, IStoreData } from "@/interfaces"
 import { AllNavbar } from "."
 
+export const revalidate = 3600
+
 async function fetchDesign () {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/design`, { cache: 'no-cache' })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/design`)
   return res.json()
 }
 
 async function fetchStoreData () {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/store-data`, { cache: 'no-cache' })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/store-data`)
   return res.json()
 }
 
